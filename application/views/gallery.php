b5b3a6e6e993681a5e9c8225a1d6664549c06cdd
priyanshu1603 <priyanshu@niktechsoftware.com>
2019-04-30 12:36:00 +0530

    gallery

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -1,33 +1,41 @@
 <div>
-	<div class="col-md-4">
-		<img src="<?php echo base_url(); ?>assets/gallery/1.jpg"> 
+	<div class="row">
+		<div class="col-md-4">
+		<img src="<?php echo base_url(); ?>assets/gallery/1.jpg" style="padding:20px;height:400px;width:85%;"> 
+	</div>
+	<div class="col-md-4" >
+		<img src="<?php echo base_url(); ?>assets/gallery/2.jpg" style="padding:20px;height:400px;width:85%;"> 
 	</div>
 	<div class="col-md-4">
-		<img src="<?php echo base_url(); ?>assets/gallery/2.jpg"> 
+		<img src="<?php echo base_url(); ?>assets/gallery/3.jpg" style="padding:20px;height:400px;width:85%;"> 
+	</div>
+	</div>
+	
+	<div class="row">
+		
+		<div class="col-md-4">
+		<img src="<?php echo base_url(); ?>assets/gallery/4.jpg" style="padding:20px;height:400px;width:85%;"> 
 	</div>
 	<div class="col-md-4">
-		<img src="<?php echo base_url(); ?>assets/gallery/3.jpg"> 
+		<img src="<?php echo base_url(); ?>assets/gallery/5.jpg" style="padding:20px;height:400px;width:85%;"> 
 	</div>
-	<div class="col-md-">
-		<img src="<?php echo base_url(); ?>assets/gallery/4.jpg"> 
+	<div class="col-md-4">
+		<img src="<?php echo base_url(); ?>assets/gallery/6.jpg" style="padding:20px;height:400px;width:85%;"> 
 	</div>
-	<div class="col-md-">
-		<img src="<?php echo base_url(); ?>assets/gallery/5.jpg"> 
 	</div>
-	<div class="col-md-">
-		<img src="<?php echo base_url(); ?>assets/gallery/6.jpg"> 
+	<div class="row">
+			<div class="col-md-4">
+		<img src="<?php echo base_url(); ?>assets/gallery/7.jpg" style="padding:20px;height:400px;width:85%;"> 
 	</div>
-	<div class="col-md-">
-		<img src="<?php echo base_url(); ?>assets/gallery/7.jpg"> 
+	<div class="col-md-4" >
+		<img src="<?php echo base_url(); ?>assets/gallery/8.jpg" style="padding:20px;height:400px;width:85%;"> 
 	</div>
-	<div class="col-md-">
-		<img src="<?php echo base_url(); ?>assets/gallery/8.jpg"> 
+	<div class="col-md-4">
+		<img src="<?php echo base_url(); ?>assets/gallery/9.jpg" style="padding:20px;height:400px;width:85%;"> 
 	</div>
-	<div class="col-md-">
-		<img src="<?php echo base_url(); ?>assets/gallery/9.jpg"> 
 	</div>
+
 	<!-- <div class="col-md-4">
 		<img src="<?php echo base_url(); ?>assets/gallery/1.jpg"> 
 	</div> -->
 	</div>
-</div>
