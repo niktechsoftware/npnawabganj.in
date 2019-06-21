afe0e9a4f33e9f6f2f9c1dfd2a16e01059d92f62
psrivastava1 <lslalitmetrix@gmail.com>
2019-06-21 13:29:37 +0530

    sd

--- application/views/chairman_massage.php
+++ application/views/chairman_massage.php
@@ -1,12 +1,12 @@
- <h2><center>Chairman Massaege</center></h2>
+ <h2><center>Chairman Massage</center></h2>
 		<div class="container">
 		<div class="row"> 
 			<div class="col-sm-8" style="border:1px solid; height:300px;">
-					<h3><p><center>President Massaege</center></h3>		
-					<h4>
+					<h3 style="font-size: 1.725em;"><p><center>President Massage</center></h3>		
+					<h3>
 						<p>मौजूदा तकनीक को ध्यान में रखते हुए हम लगातार हमारी नीतियों को नवीनीकृत करते हुए सममस्याओं के समाधान के लिए  नवाबगंज के सम्मानित नागरिकों को बेहतर और किफायती वातावरण प्रदान करते हैं| नगर पंचायत द्वारा गन्दगी से पूर्ण श्मशानों एवं कब्रिस्तानों का सुन्दरीकरण एवं उनकी जमीनों को सुरक्षित करने हेतु बाउण्ड्रीबाल आदि का निमार्ण कार्य, कराये जा रहे है। नगर पंचायत नवाबगंज’ उन्नाव में विद्युत एवं सफाई व्यवस्था के लिए ट्रेक्टर आदि क्रय किये  गये है। जनपद में सूखे की स्थिति से निपटने के लिए पानी के नये टैंकर खरीदकर नगर में पेयजल आपूर्ति को सुनिश्चित किया गया है। उपेक्षित एवं जीर्ण-शीर्ण कुओं की सफाई, मरम्मत एवं गहरीकरण कर जनता को पीने योग्य पानी की व्यवस्था की गयी है। वृक्षारोपण कार्य भी शासन के निर्देशानुसार मानकों के अनुसार किया गया है तथा अन्य ऐसे कार्य किये है जो कि इस नगर पंचायत  के कार्यकाल के विशिष्ट कार्यो में माने जाते हैं। नगर पंचायत  की सीमा विस्तार कराने के संबंध में नगर पंचायत  प्रयासरत है। 
 						</p>	
-					</h4><br>
+					</h3><br>
 
 						</p>
 					</h3>
@@ -17,7 +17,7 @@
 								<img alt="Executive Officer,Nagar Panchayat" src="<?php echo base_url();?>assets/site/writereaddata/siteContent/chairman.jpg" title="Executive Officer,Nagar Panchayat" style="height:230px;width:300px;">
 								<div class="caption">
 									<center><h3>Mr.Dileep Lashkari</h3>
-									<p>President Of Nagar Panchayat Nawabgunj</p></center>
+									<p>President Of Nagar Panchayat Nawabganj</p></center>
 								</div>
 					</div>                             
 													
